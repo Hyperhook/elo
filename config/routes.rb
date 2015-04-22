508e6006df05361638a3c04b00564e797cfe2a14
Onilol <perronium@gmail.com>
2015-04-21 22:35:31 -0300

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :players
 
-  get 'home/index'
+  get 'players/index'
 
-  root 'home#index'
+  root 'players#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
